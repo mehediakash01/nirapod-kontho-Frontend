50b4e7175fba960eca36ceed83982deef74b412c
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-03-26 14:54:22 +0600

    feat: add LoginPage and RegisterPage components with respective forms

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/src/modules/auth/components/RegisterForm";
+
+
+export default function RegisterPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+}
